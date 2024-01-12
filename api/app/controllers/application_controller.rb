7350254a69b51566799ec609b0b589ed0e10b906
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-01-12 17:32:05 +0900

    [Add] application_controllerに認証周りのコードを記述

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  # current_userやuser_signed_in?などのヘルパーメソッドへのアクセスを提供
+  ## https://devise-token-auth.gitbook.io/devise-token-auth/usage/controller_methods#methods
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # CSRF(Cross Site Request Forgery)対策を放棄
+  skip_before_action :verify_authenticity_token
 end

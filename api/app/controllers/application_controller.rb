59e6081c33787d0f839659e2df659ff806fd76fb
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-01-14 03:18:16 +0900

    [Change] リクエストを許可するドメインを変更

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
   # current_userやuser_signed_in?などのヘルパーメソッドへのアクセスを提供
   ## https://devise-token-auth.gitbook.io/devise-token-auth/usage/controller_methods#methods
   include DeviseTokenAuth::Concerns::SetUserByToken

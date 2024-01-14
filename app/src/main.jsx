9cd2609657fabd94eb2ffb3e8696bd941fefb4b6
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-01-14 12:59:25 +0900

    [Add] ユーザー登録画面のUIとユーザー登録機能を実装

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUp from './routes/SignUp.jsx'
 import Calendar from './routes/Calendar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route index element={<App />} />
+        <Route path='sign-up' element={<SignUp />} />
         <Route path='calendar' element={<Calendar />} />
       </Routes>
     </BrowserRouter>

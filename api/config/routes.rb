9dc0ccd6292e03e2b02300d9dc4dc24f6f48f31d
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-01-14 14:29:29 +0900

    [Add] calendar_eventsコントローラーのルーティングを設定

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :sessions, only: [:index]
   end
 
+  resources :calendar_events, only: [:index, :create, :update, :destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
